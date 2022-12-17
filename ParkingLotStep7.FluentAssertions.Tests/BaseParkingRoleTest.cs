f98d0a5150f90c453f144af29d339288fed2af5d
Warren HUANG <112464781+warrenhuangse@users.noreply.github.com>
2022-12-17 22:57:44 +0800

    Before FluentAssertions rewrite

---
+++ ParkingLotStep7.FluentAssertions.Tests/BaseParkingRoleTest.cs
@@ -0,0 +1,68 @@
+namespace ParkingLotStep7.FluentAssertions.Tests;
+
+public abstract class BaseParkingRoleTest<TRole, TParkable1, TParkable2> : IDisposable
+    where TRole : ParkingRole where TParkable1 : IParkable where TParkable2 : IParkable
+{
+    protected readonly Car Car;
+    protected TParkable1 Parkable1;
+    protected TParkable2 Parkable2;
+
+    protected BaseParkingRoleTest()
+    {
+        Car = new Car();
+    }
+
+    protected abstract TRole Role { get; }
+
+    public void Dispose()
+    {
+    }
+
+    [Fact]
+    public void ShouldNotParkLotWhileRoleAreFull()
+    {
+        while (!Role.IsFull())
+        {
+            Role.Park(new Car());
+        }
+        
+        Assert.Throws<Exception>(() => Role.Park(Car));
+    }
+
+    [Fact]
+    public void ShouldNotParkSameCar()
+    {
+        Role.Park(Car);
+        Assert.Throws<ArgumentException>(() => Role.Park(Car));
+
+        var secondCar = new Car();
+        Role.Park(secondCar);
+        Assert.Throws<ArgumentException>(() => Role.Park(secondCar));
+    }
+
+    [Fact]
+    public void ShouldUnparkFirstLotCar()
+    {
+        Role.Park(Car);
+        Role.Unpark(Car);
+        Assert.False(Parkable1.HasCar(Car));
+        Assert.False(Parkable2.HasCar(Car));
+    }
+
+    [Fact]
+    public void ShouldUnparkSecondLotCar()
+    {
+        Role.Park(Car);
+        var secondCar = new Car();
+        Role.Park(secondCar);
+        Role.Unpark(secondCar);
+        Assert.False(Parkable1.HasCar(secondCar));
+        Assert.False(Parkable2.HasCar(secondCar));
+    }
+
+    [Fact]
+    public void ShouldUnparkUnknownCar()
+    {
+        Assert.Throws<ArgumentException>(() => Role.Unpark(Car));
+    }
+}
---
+++ ParkingLotStep7.FluentAssertions.Tests/ParkingSuperManagerTest.cs
@@ -0,0 +1,74 @@
+namespace ParkingLotStep7.FluentAssertions.Tests;
+
+public class ParkingSuperManagerTest : BaseParkingRoleTest<ParkingSuperManager, ParkingLot, ParkingManager>
+{
+    private readonly IParkingStrategy _strategy = new FirstAvailabilityParkingStrategy();
+
+    public ParkingSuperManagerTest()
+    {
+        Parkable1 = new ParkingLot(1);
+        var boy = new ParkingBoy(new ParkingLot(1), new ParkingLot(1), _strategy);
+        Parkable2 = new ParkingManager(new ParkingLot(1), boy, _strategy);
+        Role = new ParkingSuperManager(Parkable1, Parkable2, _strategy);
+    }
+
+    protected override ParkingSuperManager Role { get; }
+
+    [Fact]
+    public void ShouldParkFirstLotFirst()
+    {
+        Role.Park(Car);
+        Assert.True(Parkable1.HasCar(Car));
+        Assert.False(Parkable2.HasCar(Car));
+    }
+
+    [Fact]
+    public void ShouldParSecondLotWhileFirstLotIsFull()
+    {
+        Role.Park(Car);
+        var secondCar = new Car();
+        Role.Park(secondCar);
+        Assert.False(Parkable1.HasCar(secondCar));
+        Assert.True(Parkable2.HasCar(secondCar));
+
+        Assert.True(Role.HasCar(Car));
+        Assert.True(Role.HasCar(secondCar));
+        Assert.Equal(2, Role.GetAvailability());
+    }
+
+    [Fact]
+    public void ShouldPrintStatus()
+    {
+        Assert.Equal(
+            "[ParkingSuperManager: 4 total, 0 occupied, 4 available"
+            + Environment.NewLine
+            + "\t[ParkingLot: 1 total, 0 occupied, 1 available],"
+            + Environment.NewLine
+            + "\t[ParkingManager: 3 total, 0 occupied, 3 available"
+            + Environment.NewLine
+            + "\t\t[ParkingLot: 1 total, 0 occupied, 1 available],"
+            + Environment.NewLine
+            + "\t\t[ParkingBoy: 2 total, 0 occupied, 2 available"
+            + Environment.NewLine
+            + "\t\t\t[ParkingLot: 1 total, 0 occupied, 1 available],"
+            + Environment.NewLine
+            + "\t\t\t[ParkingLot: 1 total, 0 occupied, 1 available]]]]",
+            Role.GetStatus());
+        Role.Park(Car);
+        Assert.Equal(
+            "[ParkingSuperManager: 4 total, 1 occupied, 3 available"
+            + Environment.NewLine
+            + "\t[ParkingLot: 1 total, 1 occupied, 0 available],"
+            + Environment.NewLine
+            + "\t[ParkingManager: 3 total, 0 occupied, 3 available"
+            + Environment.NewLine
+            + "\t\t[ParkingLot: 1 total, 0 occupied, 1 available],"
+            + Environment.NewLine
+            + "\t\t[ParkingBoy: 2 total, 0 occupied, 2 available"
+            + Environment.NewLine
+            + "\t\t\t[ParkingLot: 1 total, 0 occupied, 1 available],"
+            + Environment.NewLine
+            + "\t\t\t[ParkingLot: 1 total, 0 occupied, 1 available]]]]",
+            Role.GetStatus());
+    }
+}
